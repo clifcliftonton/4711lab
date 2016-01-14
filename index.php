2e6c916dae2f39f833642aafafbf3e4b29208a9d
Clifton Tam <ctam@live.ca>
2016-01-13 23:18:07 -0800

    update winning logic
    fix class
    clean  up

--- index.php
+++ index.php
@@ -11,15 +11,58 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        $position = filter_input(INPUT_GET,'board');
-        $squares = str_split($position);
+        class Game {
+            var $position;
+            
+            function _construct($squares) {
+                $this->position = str_split($squares);
+            }
+            
+            function winner($token, $position) {
+                for($row=0; $row<3; $row++) {
+                    $result = true;
+                    for($col=0;$col<3;$col++) {
+                        if($position[3*$row+$col] != $token) {
+                            $result = false;
+                        }
+                    }
+                }
+                return $result;
+
+            }
+            $game = new Game($squares);
+            if($game->winner('x')) {
+                echo 'You win. Lucky guesses!';
+            } else if ($game->winner('o')) {
+                echo 'I win. Muahahaha';
+            } else {
+                echo 'No winner yet.';
+            }
+                    
         
-        if(winner('x',$squares)) {
-            echo 'You win.';
-        } else if(winner('o',$squares)) {
-            echo 'I win.';
-        } else {
-            echo 'No winner.';
+            function display() {
+                echo '<table cols="3" style="font-size:large; font-weight: bold">';
+                echo '<tr>'; // open the first row
+                for($pos=0; $pos<9;$pos++) {
+                    echo $this->show_cell($pos);
+                    if($pos %3 ==2) echo '</tr><tr>';
+                }
+                echo '</tr>';
+                echo '</table>';
+            }
+
+            function show_cell($which) {
+                $token = $this->position[$which];
+                //deal with the easy case
+                if($token <> '-') {
+                    return '<td>'.$token.'</td>';
+                }
+                $this->newposition = $this->position;
+                $this->newposition[$which] = 'o';
+                $move = implode($this->newposition);
+                $link ='/?board='.$move;
+                return '<td><a href="'.$link.'">-</a></td>';
+            }
         }
 //        function qwer($token, $position) {
 //            $won = false;
@@ -30,19 +73,8 @@ and open the template in the editor.
 //            }
 //            return $won;
 //        }
+
         
-        function winner($token, $position) {
-            for($row=0; $row<3; $row++) {
-                $result = true;
-                for($col=0;$col<3;$col++) {
-                    if($position[3*$row+$col] != $token) {
-                        $result = false;
-                    }
-                }
-            }
-            return $result;
-       
-        }
         ?>
     </body>
 </html>
