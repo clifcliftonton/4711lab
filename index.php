fa7ee2c2091078e3a18f3b0a050dd0382a3855d7
Clifton Tam <ctam@live.ca>
2016-01-14 22:22:18 -0800

    clean  up issues with show_cell add ai

--- index.php
+++ index.php
@@ -14,20 +14,45 @@ and open the template in the editor.
         class Game {
             var $position;      
             var $newposition;
-            function _construct($squares) {
+            function __construct($squares) {
                 $this->position = str_split($squares);
             }         
             function winner($token) {
-                for($row=0; $row<3; $row++) {
+                $result = false;
+                if($this->position[0] == $token && $this->position[1] == $token && $this->position[2] == $token) {
                     $result = true;
-                    for($col=0;$col<3;$col++) {
-                        if($this->position[3*$row+$col] != $token) {
-                            $result = false;
-                        }
-                    }
+                } else if($this->position[3] == $token && $this->position[4] == $token && $this->position[5] == $token) {
+                    $result = true;
+                } else if($this->position[6] == $token && $this->position[7] == $token && $this->position[8] == $token) {
+                    $result = true;
+                } else if($this->position[0] == $token && $this->position[3] == $token && $this->position[6] == $token) {
+                    $result = true;
+                } else if($this->position[1] == $token && $this->position[4] == $token && $this->position[5] == $token) {
+                    $result = true;
+                } else if($this->position[2] == $token && $this->position[5] == $token && $this->position[8] == $token) {
+                    $result = true;
+                } else if($this->position[0] == $token && $this->position[4] == $token && $this->position[8] == $token) {
+                    $result = true;
+                } else if($this->position[2] == $token && $this->position[4] == $token && $this->position[6] == $token) {
+                    $result = true;
+                } else {
+                    $result = false;
                 }
                 return $result;
-            }         
+            }
+            
+                //this function is full of lies
+//            function winner($token) {   
+//                for($row=0; $row<3; $row++) {
+//                    $result = true;
+//                    for($col=0;$col<3;$col++) {
+//                        if($this->position[3*$row+$col] != $token) {
+//                            $result = false;
+//                        }
+//                    }
+//                }
+//                return $result;
+//            }         
             function display() {
                 echo '<table cols="3" style="font-size=large"; font-weight="bold">';
                 echo '<tr>'; // open the first row
@@ -46,6 +71,7 @@ and open the template in the editor.
                 if($token != '-') {
                     return '<td>'.$token.'</td>';
                 }
+                
                 $this->newposition = $this->position;
                 $this->newposition[$which] = 'o';
                 $move = implode($this->newposition);
@@ -56,14 +82,13 @@ and open the template in the editor.
         
         //main
         $squares = filter_input(INPUT_GET, 'board');
-        var_dump($squares);
         $game = new Game($squares);
         $game->display();
 
         if($game->winner('x')) {
-            echo 'You win. Lucky guesses!';
+            echo '[x] You win. Lucky guesses!';
         } else if ($game->winner('o')) {
-            echo 'I win. Muahahaha';
+            echo '[o] I win. Muahahaha';
         } else {
             echo 'No winner yet.';
         }
